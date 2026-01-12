71200d8c687cfd7a3cb24552e08e0d4ac783b8bf
LesRhabilleurs <joker2822.br@gmail.com>
2026-01-12 20:21:10 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -75,13 +75,13 @@ export default function Home() {
                 variants={fadeInUp}
                 className="flex flex-col sm:flex-row gap-4"
               >
-                <Button asChild size="lg" className="text-base">
+                <Button asChild size="lg" className="text-base"
                   <Link to="/devis">
                     Demander un devis gratuit
                     <ArrowRight className="ml-2 h-5 w-5" />
                   </Link>
                 </Button>
-                <Button asChild variant="outline" size="lg" className="text-base">
+                <Button asChild variant="outline" size="lg" className="text-base"
                   <Link to="/boutique">Découvrir nos montres</Link>
                 </Button>
               </motion.div>
