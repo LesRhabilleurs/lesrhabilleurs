a5a6f8d74fe62b542628b139746a83698125b2bb
LesRhabilleurs <joker2822.br@gmail.com>
2026-01-12 20:49:27 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -71,22 +71,7 @@ export default function Home() {
                 restauration de montres mécaniques. Expertise, passion et savoir-faire 
                 authentique depuis 2020.
               </motion.p>
-              <motion.div 
-                variants={fadeInUp}
-                className="flex flex-col sm:flex-row gap-4"
-              >
-                <Button asChild size="lg" className="text-base"
-                  <Link to="/devis">
-                    Demander un devis gratuit
-                    <ArrowRight className="ml-2 h-5 w-5" />
-                  </Link>
-                </Button>
-                <Button asChild variant="outline" size="lg" className="text-base"
-                  <Link to="/boutique">Découvrir nos montres</Link>
-                </Button>
-              </motion.div>
-            </motion.div>
-
+             
             <motion.div
               initial={{ opacity: 0, scale: 0.9 }}
               animate={{ opacity: 1, scale: 1 }}
