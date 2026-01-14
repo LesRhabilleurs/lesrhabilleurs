1375fbfb41aebb766543512cdac330b0ad376fb7
LesRhabilleurs <joker2822.br@gmail.com>
2026-01-14 18:55:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import ScrollToTop from "@/components/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 import { Layout } from "@/components/layout/Layout";
 
 import Home from "./pages/Home";
@@ -16,29 +16,32 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+const App: React.FC = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
 
-      <BrowserRouter>
-        <ScrollToTop />
+        <BrowserRouter>
+          <ScrollToTop />
 
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/boutique" element={<Shop />} />
-            <Route path="/boutique/:id" element={<WatchDetail />} />
-            <Route path="/galerie" element={<Gallery />} />
-            <Route path="/devis" element={<QuoteRequest />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/boutique" element={<Shop />} />
+              <Route path="/boutique/:id" element={<WatchDetail />} />
+              <Route path="/galerie" element={<Gallery />} />
+              <Route path="/devis" element={<QuoteRequest />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
+
 
