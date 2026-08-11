637f3baea02da06abdb816a8382b4231670dcd84
LesRhabilleurs <joker2822.br@gmail.com>
2026-08-11 20:10:29 +0200

    Fix publication dates in news items
    Updated publication dates for news items.

--- src/data/news.ts
+++ src/data/news.ts
@@ -10,7 +10,7 @@ export const newsItems: NewsItem[] = [
   {
     id: 1,
     title: "Favre-Leuba Deep Raider Day Date",
-    date: "11 août 2026",
+    date: "09 août 2026",
     image: "/article-1.jpg",
     content:
       "Favre-Leuba dévoile une nouvelle version de sa Deep Raider Day Date avec un superbe cadran vert fumé. Cette plongeuse de 40 mm, étanche à 300 mètres, conserve l'identité sportive de la collection tout en affichant le jour et la date à 3 heures. Animée par un mouvement automatique, elle est proposée sur bracelet acier et se distingue par son cadran dégradé, qui passe progressivement du vert au noir. Une nouvelle déclinaison élégante et originale pour cette collection emblématique de Favre-Leuba."
@@ -18,7 +18,7 @@ export const newsItems: NewsItem[] = [
     {
     id: 2,
     title: "Oris Star Edition",
-    date: "11 juillet 2026",
+    date: "05 juillet 2026",
     image: "/article-2.jpg",
     content:
       "Oris dévoile la nouvelle Star Edition, une montre élégante qui associe l’esprit sportif de la maison à une finition raffinée. Son design équilibré, son cadran soigneusement travaillé et ses détails soignés lui donnent une présence à la fois moderne et intemporelle. Une nouvelle édition qui vient enrichir l’univers Oris avec une pièce sobre, élégante et pleine de caractère."
